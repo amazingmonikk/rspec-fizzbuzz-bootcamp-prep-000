37944eb0b6c5eb1dffb3a9b58b44eab33a32ede0
Monika Tadross <monikk.tadross@gmail.com>
2019-03-27 21:40:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,10 +6,10 @@ def fizzbuzz(number)
  if number % 3 == 0
    "Fizz"
   elsif (number % 3 == 0) && (number % 5 == 0)
-    puts "FizzBuzz"
+     "FizzBuzz"
   elsif number % 5 == 0
-    puts "Buzz"
-   else puts nil
+     "Buzz"
+   else  nil
  end
 end
 
